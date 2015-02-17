59acc8e1f7afe34c56756d2272e3629ea68a5840
unknown <birkirpalma@gmail.com>
2015-02-17 21:11:29 +0000

    fyrsta

---
+++ views/header.php
@@ -0,0 +1,14 @@
+<?phpheader('Content-Type: text/html; charset=utf-8');?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0" />
+		<title>HSÍ</title>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="/stylesheets/style.css" />
+		<script src="scripts/JS.js"></script>
+	</head>
